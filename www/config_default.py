5a2aeda4942c7ec091658befdf27c1f32631c73b
Addison Han <addison.han@outlook.com>
2018-08-19 17:44:18 +0800

    day06

---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'Addison Han'
+
+configs = {
+    'debug':True,
+    'db':{
+        'host':'127.0.0.1',
+        'port':3306,
+        'user':'addison',
+        'password':'123456',
+        'db':'awesome'
+    },
+    'session':'Awesome'
+}
